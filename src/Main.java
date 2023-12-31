b2c0b01622631e6885368b8a84106d44c45476e3
abderrahmen bejaoui <abderrahman.bejaoui@insat.ucar.tn>
2023-12-31 14:33:57 +0100

    problem submitted

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i : nums){
+            if(map.containsKey(i)){
+                map.put(i, map.get(i) + 1);
+            }
+            else{
+                map.put(i, 1);
+            }
+        }
+        for(int i : nums){
+            if(map.get(i) == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
